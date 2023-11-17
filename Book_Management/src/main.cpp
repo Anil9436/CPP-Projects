0e992eea268aba686b6de1d21fbf5d07cb84d2ae
Anil9436 <dandugulaanil94@gmail.com>
2023-11-17 23:15:17 +0530

    intellisense issue

--- Book_Management/src/main.cpp
+++ Book_Management/src/main.cpp
@@ -4,15 +4,10 @@ sqlite3* db;
 sqlite3_stmt* stmt;
 int main()
 {
-
     BookDetails Bfirst{};
     EmployeeDetails Efirst{};
     Suppliers Sfirst{};
     Bfirst.CreateDB1();
     Sfirst.CreateDB2();
-    Efirst.CreateDB3();
-    Bfirst.add();
-    Bfirst.display();
-    
-    
+    Efirst.CreateDB3(); 
 }
